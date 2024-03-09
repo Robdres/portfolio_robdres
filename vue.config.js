82d94a7e33ed11db4a22ea1e2415e46171436fda
Robdres <robdres123@gmail.com>
2024-03-09 10:03:11 +0000

    Vue commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,3 @@
-const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  publicPath: process.env.NODE_ENV === "production" ? "/portfolio_robdres/" : "/",
 })
